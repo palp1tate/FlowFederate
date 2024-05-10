6e7cc972a8eb5fa878b662d83b601ee7eca912ac
wxy <1939311091@qq.com>
2024-05-10 16:37:39 +0800

    Add Docker setup for client and edge services
    Added Dockerfile and run.sh scripts for client and edge services, enabling an easier setup process. These service configurations were also integrated into the docker-compose file to allow them to join the existing architecture. Environment, build context, and dependencies were taken into account for the configuration.

---
+++ service/edge/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# 尝试连接到 MySQL
+while ! nc -z mysql 3306; do
+  echo "Waiting for MySQL..."
+  sleep 1
+done
+
+echo "MySQL is up..."
+
+# 尝试连接到 Consul
+while ! nc -z consul 8500; do
+  echo "Waiting for Consul..."
+  sleep 1
+done
+
+echo "Consul is up..."
+
+# 设置 PYTHONPATH 环境变量
+export PYTHONPATH=./
+
+# 执行 python 程序
+python ./service/edge/edge.py "$1" "$2"
+
