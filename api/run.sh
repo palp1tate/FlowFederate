3e48322f68b6c5de734efdb63eca93aa10ea99b9
wxy <1939311091@qq.com>
2024-05-10 11:04:39 +0800

    Add consul connection check to run.sh script
    The existing run.sh script was improved for checking the connection to not only MySQL but also Consul. The environment variables for MySQL host and port have been removed in favor of hardcoded values. The script will now output proper checks and establish connection to both MySQL on port 3306 and Consul on port 8500 respectively before running the main program.

--- api/run.sh
+++ api/run.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-# 环境变量 MYSQL_HOST 和 MYSQL_PORT 应该设置为 MySQL 服务器的主机名和端口
-host="${MYSQL_HOST:-mysql}"
-port="${MYSQL_PORT:-3306}"
-
 # 尝试连接到 MySQL
-while ! nc -z "$host" "$port"; do
-  echo "Waiting for MySQL at $host:$port..."
+while ! nc -z mysql 3306; do
+  echo "Waiting for MySQL..."
   sleep 1
 done
 
 echo "MySQL is up..."
+
+# 尝试连接到 Consul
+while ! nc -z consul 8500; do
+  echo "Waiting for Consul..."
+  sleep 1
+done
+
+echo "Consul is up..."
 
 # 执行 main 程序
 ./main -p 9090
