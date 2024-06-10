94c319351eb04899838afa94743b5239e80aa600
LiZaiyuan0619 <121675297+LiZaiyuan0619@users.noreply.github.com>
2024-06-10 19:36:44 +0800

    Add files via upload
    两个dataset.py是相同的改进

---
+++ client.py
@@ -0,0 +1,289 @@
+import argparse
+import io
+import json
+import logging
+import uuid
+from concurrent import futures
+from datetime import datetime
+
+import grpc
+import torch
+from sqlalchemy.orm import sessionmaker
+from torch.utils.data import DataLoader
+
+import client_pb2
+import client_pb2_grpc
+from consulx import Consul
+from datasets import get_dataset
+from get_model import get_model
+from internal.model.table import Client
+from util import *
+
+conf = config.load_configuration("service/client/config.yaml")
+client_credentials = config.load_client_credentials("internal/authorization")
+server_credentials = config.load_server_credentials("internal/authorization")
+service_conf = conf["service"]
+consul_conf = conf["consul"]
+
+# Set up basic logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+STREAM_PART_SIZE = 2 * 1024 * 1024
+MAX_MESSAGE_LENGTH = 1024 * 1024 * 1024
+options = [
+    ("grpc.max_send_message_length", MAX_MESSAGE_LENGTH),
+    ("grpc.max_receive_message_length", MAX_MESSAGE_LENGTH),
+    ("grpc.enable_retries", 1),
+]
+COMMON_NAME = conf["common_name"]
+server = None
+service_id = str(uuid.uuid4())
+consul = Consul(consul_host=consul_conf["host"], consul_port=consul_conf["port"])
+engine = init_engine()
+
+
+def train_model(pt: bytes, configuration: dict, task_id: int) -> bytes:
+    try:
+        model_name = configuration["model_name"]
+        dataset_type = configuration["type"]
+        local_epochs = configuration["local_epochs"]
+        batch_size = configuration["batch_size"]
+        learning_rate = configuration["lr"]
+        momentum = configuration["momentum"]
+        new_session = sessionmaker(engine)
+        with new_session() as session:
+            client = (
+                session.query(Client)
+                .filter_by(task_id=task_id, client_id=service_id)
+                .first()
+            )
+            if not client:
+                client = Client(
+                    task_id=task_id,
+                    client_id=service_id,
+                    created_at=datetime.now(),
+                    updated_at=datetime.now(),
+                    model=model_name,
+                    dataset=dataset_type,
+                    type="通用模型",
+                    status="正常",
+                    current_round=0,
+                    total_round=local_epochs,
+                    cpu=get_cpu_usage(),
+                    memory=get_memory_usage(),
+                    disk=get_disk_usage(),
+                    progress="0%",
+                )
+                session.add(client)
+                session.commit()
+
+        logging.info("Training model...")
+        state_dict = torch.load(io.BytesIO(pt))
+
+        logging.info(
+            f"Model: {model_name}, Dataset: {dataset_type}, Local epochs: {local_epochs}, Batch size: {batch_size}, "
+            f"Learning rate: {learning_rate}, Momentum: {momentum}"
+        )
+
+        local_model = get_model(model_name)
+
+        local_model.load_state_dict(state_dict)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        local_model = local_model.to(device)
+
+        train_datasets, eval_datasets = get_dataset("service/client/data", dataset_type)
+        train_loader = DataLoader(train_datasets, batch_size=batch_size, shuffle=True)
+        eval_loader = DataLoader(eval_datasets, batch_size=batch_size, shuffle=False)
+
+        optimizer_name = configuration.get("optimizer", "sgd")  # 默认使用SGD
+        if optimizer_name == "sgd":
+            optimizer = torch.optim.SGD(
+                local_model.parameters(), lr=learning_rate, momentum=momentum
+            )
+        elif optimizer_name == "adam":
+            # optimizer = torch.optim.Adam(
+            #     local_model.parameters(), lr=learning_rate, betas=(0.9, 0.999)
+            # )
+            optimizer = torch.optim.SGD(
+                local_model.parameters(), lr=learning_rate, momentum=momentum
+            )
+        elif optimizer_name == "rmsprop":
+            # optimizer = torch.optim.RMSprop(
+            #     local_model.parameters(), lr=learning_rate, alpha=0.99
+            # )
+            optimizer = torch.optim.SGD(
+                local_model.parameters(), lr=learning_rate, momentum=momentum
+            )
+        else:
+            raise ValueError(f"Unsupported optimizer type: {optimizer_name}")
+        logging.info(f"Optimizer: {optimizer_name}")
+
+        loss_function_name = configuration.get(
+            "loss_function", "cross_entropy"
+        )  # 默认为交叉熵损失
+        if loss_function_name == "cross_entropy":
+            criterion = torch.nn.CrossEntropyLoss()
+        elif loss_function_name == "mse":
+            # criterion = torch.nn.MSELoss()
+            criterion = torch.nn.CrossEntropyLoss()
+        elif loss_function_name == "nll":
+            # criterion = torch.nn.NLLLoss()
+            criterion = torch.nn.CrossEntropyLoss()
+        else:
+            raise ValueError(f"Unsupported loss function type: {loss_function_name}")
+        logging.info(f"Loss function: {loss_function_name}")
+
+        local_epochs = 1
+
+        for epoch in range(local_epochs):
+            local_model.train()
+            total_train_loss = 0
+            for data, target in train_loader:
+                data, target = data.to(device), target.to(device)
+                optimizer.zero_grad()
+                _, output = local_model(data)
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()
+                total_train_loss += loss.item()
+
+            # 计算平均训练损失
+            avg_train_loss = total_train_loss / len(train_loader)
+
+            # 评估模型
+            local_model.eval()
+            total_eval_loss = 0
+            correct = 0
+            with torch.no_grad():
+                for data, target in eval_loader:
+                    data, target = data.to(device), target.to(device)
+                    _, output = local_model(data)
+                    loss = criterion(output, target)
+                    total_eval_loss += loss.item()
+                    pred = output.argmax(dim=1, keepdim=True)  # 获取预测结果
+                    correct += pred.eq(target.view_as(pred)).sum().item()
+
+            # 计算平均验证损失和精度
+            avg_eval_loss = total_eval_loss / len(eval_loader)
+            accuracy = correct / len(eval_loader.dataset)
+
+            print(
+                f"Epoch {epoch + 1}/{local_epochs}, Train Loss: {avg_train_loss:.4f}, Eval Loss: {avg_eval_loss:.4f}, "
+                f"Accuracy: {accuracy:.4f}"
+            )
+
+            with new_session() as session:
+                client = (
+                    session.query(Client)
+                    .filter_by(task_id=task_id, client_id=service_id)
+                    .first()
+                )
+                if client:
+                    client.updated_at = datetime.now()
+                    client.current_round = epoch + 1
+                    client.accuracy = accuracy
+                    client.loss = avg_eval_loss
+                    client.cpu = get_cpu_usage()
+                    client.memory = get_memory_usage()
+                    client.disk = get_disk_usage()
+                    client.progress = f"{(epoch + 1) * 100 // local_epochs}%"
+                    client.status = "正常"
+                    session.commit()
+
+        trained_model = local_model.state_dict()
+        buf = io.BytesIO()
+        torch.save(trained_model, buf)
+        return buf.getvalue()
+    except Exception as exc:
+        new_session = sessionmaker(engine)
+        with new_session() as session:
+            client = (
+                session.query(Client)
+                .filter_by(task_id=task_id, client_id=service_id)
+                .first()
+            )
+            if client is not None:
+                client.status = "异常"
+                client.updated_at = datetime.now()
+                session.commit()
+        logging.error(f"Model training failed: {exc}")
+        raise
+
+
+class ClientServicer(client_pb2_grpc.ClientServiceServicer):
+
+    def TrainModel(self, request_iterator, context):
+        try:
+            pt = bytearray()
+            conf_json = None
+            task_id = None
+            for TrainRequest in request_iterator:
+                pt_size = TrainRequest.size
+                pt_data = TrainRequest.pt
+                if conf_json is None:
+                    conf_json = json.loads(TrainRequest.conf)
+                if task_id is None:
+                    task_id = TrainRequest.task_id
+                pt.extend(pt_data)
+                if len(pt) == pt_size:
+                    break
+            logging.info(f"Preparing to train model...")
+            new_pt = train_model(pt=pt, configuration=conf_json, task_id=task_id)
+            file_size = len(new_pt)  # 获取pt的大小
+            with io.BytesIO(new_pt) as stream:
+                while True:
+                    chunk = stream.read(STREAM_PART_SIZE)
+                    if chunk:
+                        yield client_pb2.TrainModelResponse(pt=chunk, size=file_size)
+                    else:
+                        break
+        except json.JSONDecodeError as exception:
+            logging.error(f"JSON decoding error: {exception}")
+            context.set_details("Invalid JSON configuration.")
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            return client_pb2.TrainModelResponse()
+        except Exception as exception:
+            logging.error(f"Error during model training: {exception}")
+            context.set_details(f"An error occurred during model training: {exception}")
+            context.set_code(grpc.StatusCode.INTERNAL)
+            return client_pb2.TrainModelResponse()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Start the server with specified port")
+    parser.add_argument(
+        "-p", "--port", type=int, help="The port to start the server on"
+    )
+    args = parser.parse_args()
+    port = args.port if args.port else get_free_port()
+    try:
+        server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=1000), options=options
+        )
+        client_pb2_grpc.add_ClientServiceServicer_to_server(ClientServicer(), server)
+        server_address = f"[::]:{port}"
+        server.add_secure_port(server_address, server_credentials)
+        server_host = get_ip_address()
+        server.start()
+        logging.info(f"Service started at {server_host}:{port}")
+        consul.register(
+            address=server_host,
+            port=port,
+            service_name=service_conf["name"],
+            tags=service_conf["tags"],
+            service_id=service_id,
+        )
+        logging.info(f"Service registered with Consul successfully.")
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        server.stop(0)
+        logging.info("Server stopped by KeyboardInterrupt.")
+        consul.deregister(server_id=service_id)
+        logging.info(f"Service unregistered from Consul successfully.")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        raise
---
+++ datasets.py
@@ -0,0 +1,221 @@
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import Subset
+from torchvision import datasets
+from torchvision import transforms
+
+
+seed = 42
+
+
+def get_subset(dataset, fraction=0.1):
+    """
+    获取数据集的子集
+    :param dataset: 原始数据集
+    :param fraction: 子集占原始数据集的比例
+    :return: 数据集的子集
+    """
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    subset_size = int(dataset_size * fraction)
+
+    # 随机选择子集
+    np.random.shuffle(indices)
+    subset_indices = indices[:subset_size]
+
+    return Subset(dataset, subset_indices)
+
+
+def get_dataset(dir, name):
+    if name == "mnist":
+        train_dataset = datasets.MNIST(
+            dir, train=True, download=True, transform=transforms.ToTensor()
+        )
+        eval_dataset = datasets.MNIST(dir, train=False, transform=transforms.ToTensor())
+
+    if name == "fashionmnist":
+        train_dataset = datasets.FashionMNIST(
+            dir, train=True, download=True, transform=transforms.ToTensor()
+        )
+        eval_dataset = datasets.FashionMNIST(
+            dir, train=False, transform=transforms.ToTensor()
+        )
+        print("get_fashionmnist")
+
+    elif name == "cifar10":
+        transform_train = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
+
+        transform_test = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
+
+        train_dataset = datasets.CIFAR10(
+            dir, train=True, download=True, transform=transform_train
+        )
+        eval_dataset = datasets.CIFAR10(dir, train=False, transform=transform_test)
+
+    elif name == "cifar100":
+        transform_train = transforms.Compose(
+            [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
+
+        transform_test = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
+
+        train_dataset = datasets.CIFAR100(
+            dir, train=True, download=True, transform=transform_train
+        )
+        eval_dataset = datasets.CIFAR100(dir, train=False, transform=transform_test)
+
+    train_subset = get_subset(train_dataset, fraction=0.2)
+    eval_subset = get_subset(eval_dataset, fraction=0.2)
+
+    return train_subset, eval_subset
+
+# *生成一组非独立同分布（non-IID）的数据集，其中每个客户端的数据集包含了多个类别的样本。
+# *深度学习中，非独立同分布（non-IID）数据集是指数据集中的样本分布不同于整个数据集的分布。如果数据集是非独立同分布的，模型就可能无法很好地学习到数据集的分布特征，从而影响模型的性能和泛化能力。
+# *在实际应用中，由于数据来源的多样性和数据采集的不确定性，很多数据集都是非独立同分布的。在联合学习等场景中，需要将非独立同分布的数据集分配给不同的客户端进行训练，以提高模型的泛化能力和性能。
+def get_nonIID_data(conf):
+    # *用于存储每个客户端的数据集合
+    client_idx = {}
+    # *包含所有类别的列表all_data，其中每个元素代表一个类别
+    all_data = []
+    for i in range(conf["classes"]):
+        all_data.append(i)
+    for i in range(conf["clients"]):
+        # *从 all_data 中随机选择 conf["client_classes"] 个类别，生成了一个包含了多个类别的样本集合 samples
+        # *将 samples 添加到 client_idx 中，生成了一个多个类别的样本集合。最终，代码返回了 client_idx，其中包含了所有客户端的数据集合。
+        # *这里使用replace=False参数来确保每个类别只被选择一次，避免重复
+        samples = np.random.choice(all_data, size=conf["client_classes"], replace=False)
+        client_idx[i + 1] = samples
+
+    # *返回client_idx字典，其中包含了所有客户端的数据集合。每个客户端的数据集合是一个包含多个类别的样本集合，用于构建非独立同分布的数据集。
+    # *也就是说这个函数完成的任务是：传入配置信息，执行非独立同分布数据集合，每个客户端都有随机选取的配置信息指定数目的size=conf["client_classes"]个data
+    return client_idx
+
+
+def dirichlet_split_noniid(train_labels, alpha, n_clients):
+    n_classes = train_labels.max() + 1
+    # (K, N) 类别标签分布矩阵X，记录每个类别划分到每个client去的比例
+    # *使用Dirichlet分布生成标签分布，以便在联邦学习中模拟非独立同分布（Non-IID）数据
+    # *np.random.dirichlet()是NumPy库中的函数，用于从Dirichlet分布中抽取样本。在这里，np.random.dirichlet([alpha] * n_clients, n_classes)
+    # *生成一个二维形状为(n_clients, n_classes)的NumPy数组，每个元素表示每个客户端在每个类别上的标签分布。每一行表示一个客户端的标签分布，每一列表示一个类别的概率，
+    # *alpha是Dirichlet分布的参数，用于控制标签分布的平滑度。
+    # *Dirichlet分布是一种连续多元概率分布，通常用于对未知概率向量的不确定性建模，Dirichlet分布样本是一个概率向量，其各个分量之和为1
+    label_distribution = np.random.dirichlet([alpha] * n_clients, n_classes)
+    # label_distribution = np.random.dirichlet(np.repeat(alpha, n_clients))
+    # (K, ...) 记录K个类别对应的样本索引集合
+    # *将训练集中每个类别的标签索引存储在一个列表中，列表中的第i个元素是一个包含训练集中标签为i的样本索引的NumPy数组。np.argwhere()返回满足条件的元素的索引
+    # *np.argwhere(train_labels == y)返回一个形状为(m, 1)的NumPy数组，其中m是训练集中标签为y的样本数量，每个元素是一个标签为y的样本的索引,然后展平为一维数组
+
+    """
+    例如，如果训练标签中有100个样本，其中有30个属于类别y，那么类别y的样本索引数组就是一个包含30个元素的一维数组，表示这30个样本在训练数据集中的索引位置。
+    """
+    class_idcs = [np.argwhere(train_labels == y).flatten() for y in range(n_classes)]
+    # 记录N个client分别对应的样本索引集合
+    # *使用zip(class_idcs, label_distribution)将标签索引和标签分布进行迭代。在每次迭代中，k_idcs表示类别k的样本索引数组，fracs表示标签k在每个客户端上的样本比例。
+    # *client_idcs列表被初始化为包含了n_clients个空列表的列表。每个空列表都用于存储一个客户端对应的样本索引集合。
+    client_idcs = [[] for _ in range(n_clients)]
+    for k_idcs, fracs in zip(class_idcs, label_distribution):
+        # np.split按照比例fracs将标签为k的样本索引k_idcs划分为了N个子集
+        # i表示第i个client，idcs表示其对应的样本索引集合idcs
+        # *将标签k的样本索引数组按照比例fracs划分为多个子集。这里使用np.cumsum(fracs)[:-1] * len(k_idcs)计算每个划分点的位置，然后使用np.split()函数进行划分。
+        # *划分后的每个子集都是一个客户端对应的样本索引数组。
+        # *`enumerate()`函数同时获取索引`i`和划分后的子集样本索引数组`idcs`，然后将`idcs`添加到`client_idcs`列表中。
+        for i, idcs in enumerate(
+            np.split(k_idcs, (np.cumsum(fracs)[:-1] * len(k_idcs)).astype(int))
+        ):
+            # *每个客户端对应的样本索引数组添加到client_idcs列表中。
+            client_idcs[i] += [idcs]
+    # *将每个客户端的样本索引数组连接起来，得到一个包含每个客户端样本索引的列表
+    # *通过列表推导式[np.concatenate(idcs) for idcs in client_idcs]，遍历client_idcs列表中的每个元素idcs，
+    # *使用np.concatenate()函数将其中的样本索引数组连接起来。
+    # *最终，得到的client_idcs列表中的每个元素都是一个包含了一个客户端对应的样本索引的一维数组。
+    # *这个操作之前client_idcs是一个二维列表，其中每个元素都是一个包含了一个客户端对应的样本索引的一维数组。
+    client_idcs = [np.concatenate(idcs) for idcs in client_idcs]
+    # return client_idcs
+    # *每个客户端的样本索引数组存储到一个字典client_idx中，其中键是客户端的编号，值是客户端对应的样本索引数组。
+    client_idx = {}
+    for i in range(len(client_idcs)):
+        client_idx[i + 1] = client_idcs[i]
+    return client_idx
+
+
+def dirichlet_nonIID_data(train_data, conf):
+    # *NumPy库中的函数，用于设置随机数生成器的种子(seed)值。其中，seed是一个整数，用于指定随机数生成器的种子值。
+    # *这个种子值可以使随机数生成器的输出可预测，因为每次使用相同的种子值，都会得到相同的随机数序列
+    np.random.seed(seed)
+
+    # *得到传入参数——训练数据集的类别
+    classes = train_data.classes
+    # *获得类别总数
+    n_classes = len(classes)
+    # *注释代码作用是：将PyTorch数据集中的训练集和测试集的标签合并为一个NumPy数组，标签沿着0轴连接
+    # labels = np.concatenate([np.array(train_data.targets), np.array(test_data.targets)], axis=0)
+    # *数据集中的标签转换为NumPy数组
+    labels = np.array(train_data.targets)
+    # dataset = ConcatDataset([train_data, test_data])
+
+    # 我们让每个client不同label的样本数量不同，以此做到Non-IID划分
+    # client_idcs = dirichlet_split_noniid(labels, alpha=conf["dirichlet_alpha"], n_clients=conf["clients"])
+    return dirichlet_split_noniid(
+        labels, alpha=conf["dirichlet_alpha"], n_clients=conf["clients"]
+    )
+
+    # 展示不同label划分到不同client的情况
+    fig = plt.figure(figsize=(8, 7))
+    plt.hist(
+        [labels[idc] for idc in client_idcs],
+        stacked=True,
+        bins=np.arange(min(labels) - 0.5, max(labels) + 1.5, 1),
+        label=["Terminal {}".format(i) for i in range(conf["clients"])],
+        rwidth=0.8,
+    )
+    # plt.xticks(np.arange(n_classes), fontsize=20)
+    plt.xticks(np.array([0, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99]), fontsize=20)
+    plt.xlabel("Label type", fontsize=20)
+    plt.ylabel("Number of samples", fontsize=20)
+    plt.legend(loc="upper right")
+    plt.title("CIFAR100", fontsize=20)
+    plt.show()
+
+    filename = "Lai-CIFAR100.pdf"
+    fig.savefig("../result/" + filename, bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    with open("../utils/conf.json", "r") as f:
+        conf = json.load(f)
+
+    train_datasets, eval_datasets = get_dataset("../data/", "cifar100")
+
+    dirichlet_nonIID_data(train_datasets, conf)
